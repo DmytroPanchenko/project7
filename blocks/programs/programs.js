5a912156b20ad3442877d390a7cd8c48e430ddfa
Dmytro Panchenko <dmytro_panchenko@epam.com>
2023-09-06 19:00:48 -0400

    Added programs component; fixed the URL for non-Production instances

---
+++ blocks/programs/programs.js
@@ -0,0 +1,32 @@
+export default function decorate(block) {
+    // Change the structure of the section by removing the intermediate <div>-s
+    const intermediateDiv = block.querySelectorAll(':scope > div');
+
+    intermediateDiv.forEach(rowDiv => {
+        Array.from(rowDiv.children).forEach(tile => {
+             block.appendChild(tile);
+        });
+        block.removeChild(rowDiv);
+    });
+
+    const cards = block.querySelectorAll(':scope > div');
+    cards.forEach(card => {
+        const lastParagraph = card.lastElementChild;
+        const link = lastParagraph.querySelector(':scope > a');
+
+        lastParagraph.removeChild(link);
+        link.innerText = "Learn more";
+        link.classList.add("button");
+        link.classList.add("secondary");
+
+        card.appendChild(link);
+        card.removeChild(lastParagraph);
+//        console.log(`+++ ${link}`);
+//        Array.from(card.children).forEach(tile => {
+//            console.log(`+++ ${tile.tagName}`);
+//            block.appendChild(tile);
+//        });
+//        block.removeChild(rowDiv);
+    });
+
+}
--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,6 +1,14 @@
 // eslint-disable-next-line import/no-cycle
 import { sampleRUM } from './lib-franklin.js';
 
+const PROD_SITE = 'https://www.mapeuniversity.com/';
+
+document.querySelectorAll('a').forEach((a) => {
+    if (a.href.indexOf(PROD_SITE) === 0) {
+        a.href = a.href.replace(PROD_SITE, '/');
+    }
+});
+
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
