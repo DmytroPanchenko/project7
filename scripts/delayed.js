6903343fd3af2974218cceff0df07a93f06718da
Dmytro Panchenko <dmytro_panchenko@epam.com>
2023-10-06 18:40:18 -0400

    Added Adobe Launch code

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -3,6 +3,9 @@ import { sampleRUM } from './lib-franklin.js';
 
 const PROD_SITE = 'https://www.mapeuniversity.com/';
 
+// Adobe Launch
+_satellite.pageBottom();
+
 document.querySelectorAll('a').forEach((a) => {
     if (a.href.indexOf(PROD_SITE) === 0) {
         a.href = a.href.replace(PROD_SITE, '/');
