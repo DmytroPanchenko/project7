1e66a9c6fad6a6449bbd82ce793f88f739fb2e7b
Dmytro Panchenko <dmytro_panchenko@epam.com>
2023-10-08 17:18:45 -0400

    Added on click handler

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,6 +1,6 @@
 export default function decorate(block) {
     // Select all child div elements of .hero.block
-    const heroBlocks = document.querySelectorAll('.hero.block > div');
+    const heroBlocks = block.querySelectorAll(':scope > div');
 
 // Loop over each heroBlock
     heroBlocks.forEach(function(block) {
@@ -9,4 +9,14 @@ export default function decorate(block) {
             block.classList.add('textcontent');
         }
     });
+
+    const buttons = block.querySelectorAll(':scope .button.primary');
+    buttons.forEach(function(btn) {
+        btn.classList.add('btnconversion');
+        btn.addEventListener("click", function(e) {
+            alert("Not implemented");
+        });
+    });
+
+
 }
